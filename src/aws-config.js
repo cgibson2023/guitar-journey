a2095803a86de307fca398191f171cd79540429c
cgibson2023 <virtualgibby@gmail.com>
2026-02-05 22:56:58 -0500

    Update identity pool ARN

--- src/aws-config.js
+++ src/aws-config.js
@@ -6,7 +6,7 @@ Amplify.configure({
       // REPLACE THESE WITH YOUR ACTUAL VALUES FROM COGNITO
       userPoolId: 'us-east-1_1brwcPto0',     // Found in Cognito User Pool → General Settings
       userPoolClientId: 'mhclh9sd6toga09j61nppms47', // Found in Cognito User Pool → App clients
-      identityPoolId: 'us-east-1:abc12345-1234-1234-1234-123456789012', // Found in Cognito → Identity Pools
+      identityPoolId: 'us-east-1:0f75e6c4-4f9b-4ff7-9b07-11d948655b27', // Found in Cognito → Identity Pools
       loginWith: {
         email: true,
       },
